80266329859b3c3b26336c682d26cb54982abe56
Simounet <contact@simounet.net>
2014-11-23 18:58:04 +0100

    Smart feeds update option added to the settings page

--- smartfeedsupdate.plugin.php
+++ smartfeedsupdate.plugin.php
@@ -9,5 +9,33 @@
 @description Better feeds update handler
 */
 
+require_once( __DIR__ . "/classes/SmartFeedsUpdate.php" );
+
+function add_synchro_option( $synchronisationType ) {
+    $identifier = 'smartupdate';
+
+    $checked = false;
+    if( $synchronisationType == $identifier ) {
+        $checked = 'checked="checked"';
+    }
+
+    echo
+        '<p>' .
+            '<input
+                type="radio"
+                id="synchronisationType' . $identifier . '"
+                name="synchronisationType"
+                value="' . $identifier . '" ' .
+                $checked .
+            '> '.
+            '<label
+                for="synchronisationType' . $identifier .'">' .
+                '<strong>' . _t('SMARTFEEDUPDATE_SMART') . ' :</strong>' .
+            '</label> ' . _t('SMARTFEEDUPDATE_SMART_DESC') .
+        '</p>';
+}
+
+Plugin::addHook( "setting_post_synchronisation_options", "add_synchro_option" );
+
 
 ?>
